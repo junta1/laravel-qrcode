73903c3dc388ecbc3556b635b855e40315a0b805
João Victor Andrade Rosendo da Paz <joaopaz@C0116.conder.intranet>
2020-03-09 14:14:37 -0300

    implementações de classes

--- src/resources/views/import/create.blade.php
+++ src/resources/views/import/create.blade.php
@@ -5,14 +5,54 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
+
+<style>
+
+* {
+    margin:0;
+    padding:0;
+}
+
+.container {
+    width: 100%;
+    height:100%;
+}
+
+    .btn {
+        padding: 8px;
+        border: 1px solid #aaa;
+        background-color: #f8f9fa;
+        border-radius: 6px;
+        outline: none;
+    }
+
+    .btn:focus {
+        border: 1px solid #5f5f5f;
+        transition: 0.50s;
+        box-shadow: 0 0 2px #655e5e;
+    }
+
+    .btn:hover {
+        background-color: #e2e6ea;
+        cursor: pointer;
+    }
+
+</style>
+
 <body>
 
+<div class="container">
 {!! Form::open(array('route' => 'import.store','method' => 'post', 'files' => true, 'target' => '_blank')) !!}
 
 {!! Form::file('file') !!}
 
-{!! Form::submit('Cadastrar',array('class' => 'btn btn-success', )) !!}
+{!! Form::submit('Cadastrar',array('class' => 'btn')) !!}
 
 {!! Form::close() !!}
+</div>
+
+
+
 </body>
+
 </html>
